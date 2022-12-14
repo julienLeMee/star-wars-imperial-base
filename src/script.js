beea147d26ab000f19b172de80a8fb4d39ae91c7
Julien Le Mee <julemee@gmail.com>
2022-12-14 15:29:46 -0500

    added cockpit and wings

--- src/script.js
+++ src/script.js
@@ -73,18 +73,68 @@ window.addEventListener('dblclick', () =>
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// Floor
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20), // géométrie du plan
+  new THREE.MeshBasicMaterial({
+    color: '#ff0000',
+    side: THREE.DoubleSide
+  }) // matériau du plan
 )
-scene.add(mesh)
+floor.rotation.x = - Math.PI * 0.5 // rotation du plan, pour qu'il soit horizontal
+floor.position.y = 0 // position du plan sur l'axe y (hauteur)
+scene.add(floor)
+
+
+/**
+ * Objects
+ */
+
+// Tie fighter
+const tieFighter = new THREE.Group()
+scene.add(tieFighter)
+
+// Cockpit
+const cockpitGeometry = new THREE.SphereGeometry( 0.5, 16, 16 );
+const cockpitMaterial = new THREE.MeshBasicMaterial( {
+  color: 0xffff00,
+  wireframe: true
+});
+const cockpit = new THREE.Mesh( cockpitGeometry, cockpitMaterial );
+cockpit.position.y = 2 // position du cockpit sur l'axe y (hauteur)
+tieFighter.add( cockpit );
+
+// Wings
+const wingGeometry = new THREE.PlaneGeometry( 2, 2);
+const wingMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+  side: THREE.DoubleSide,
+  wireframe: true
+});
+const wing = new THREE.Mesh( wingGeometry, wingMaterial );
+wing.rotation.y = - Math.PI * 0.5
+wing.position.x = 1 // position de l'aile sur l'axe x (largeur)
+wing.position.y = 2 // position de l'aile sur l'axe y (hauteur)
+tieFighter.add( wing );
+
+const wing2Geometry = new THREE.PlaneGeometry( 2, 2 );
+const wing2Material = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+  side: THREE.DoubleSide,
+  wireframe: true
+});
+const wing2 = new THREE.Mesh( wing2Geometry, wing2Material );
+wing2.rotation.y = - Math.PI * 0.5
+wing2.position.x = -1 // position de l'aile sur l'axe x (largeur)
+wing2.position.y = 2 // position de l'aile sur l'axe y (hauteur)
+tieFighter.add( wing2 );
+
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-// const aspectRatio = sizes.width / sizes.height
-// const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100) // caméra en perspective, avec un champ de vision de 75°, une largeur et une hauteur de la fenêtre du navigateur, une distance minimale de 0.1 et une distance maximale de 100
+camera.position.x = 4 // position de la caméra sur l'axe x
+camera.position.y = 2 // position de la caméra sur l'axe y
+camera.position.z = 5 // position de la caméra sur l'axe z
 scene.add(camera)
 
 // Controls
@@ -93,7 +143,8 @@ controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 
