de18002fb19d383c23f54fdd1be569a1d3f1c2b4
Julien Le Mee <julemee@gmail.com>
2022-12-20 00:18:35 -0500

    added torch light

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
+// import * as dat from 'lil-gui'
 
-// Debug
-const gui = new dat.GUI({ width: 340 })
+// // Debug
+// const gui = new dat.GUI({ width: 340 })
 
 /**
  * Base
@@ -86,6 +86,8 @@ scene.add(camera)
 
 // Textures
 const textureLoader = new THREE.TextureLoader()
+
+const alphaMap = textureLoader.load('/textures/alphamap.png') // charge la texture
 
 // floor texture
 const floorColorTexture = textureLoader.load('/textures/metal/basecolor.jpg')
@@ -367,6 +369,121 @@ for (let i = 0; i < 25; i++) {
 }
 
 /**
+ * Torches
+ */
+
+const torch = new THREE.Group()
+scene.add(torch)
+
+const torchGeometry = new THREE.CylinderGeometry( 0.1, 0.1, 4, 32 ); // géométrie du cylindre (rayon du haut, rayon du bas, hauteur, nombre de segments)
+const torchMaterial = new THREE.MeshStandardMaterial({
+  color: 0x000000,
+  transparent: true,
+  roughness: 0.1, // brillance
+  metalness: 0.1, // métal
+  opacity: 0.5,
+  side: THREE.DoubleSide
+})
+const torch1 = new THREE.Mesh( torchGeometry, torchMaterial );
+torch1.position.x = 24.9 // position de l'aile sur l'axe x (largeur)
+torch1.position.y = 2 // position de l'aile sur l'axe y (hauteur)
+torch1.position.z = 24.9 // position de l'aile sur l'axe y (hauteur)
+torch.add( torch1 );
+
+const torch2 = new THREE.Mesh( torchGeometry, torchMaterial );
+torch2.position.x = -24.9 // position de l'aile sur l'axe x (largeur)
+torch2.position.y = 2 // position de l'aile sur l'axe y (hauteur)
+torch2.position.z = 24.9 // position de l'aile sur l'axe y (hauteur)
+torch.add( torch2 );
+
+const torch3 = new THREE.Mesh( torchGeometry, torchMaterial );
+torch3.position.x = 24.9 // position de l'aile sur l'axe x (largeur)
+torch3.position.y = 2 // position de l'aile sur l'axe y (hauteur)
+torch3.position.z = -24.9 // position de l'aile sur l'axe y (hauteur)
+torch.add( torch3 );
+
+const torch4 = new THREE.Mesh( torchGeometry, torchMaterial );
+torch4.position.x = -24.9 // position de l'aile sur l'axe x (largeur)
+torch4.position.y = 2 // position de l'aile sur l'axe y (hauteur)
+torch4.position.z = -24.9 // position de l'aile sur l'axe y (hauteur)
+torch.add( torch4 );
+
+// head torch
+// ajouter un Octahedron pour faire une tête de torche
+const headTorchGeometry = new THREE.OctahedronGeometry( 0.2, 0 ); // rayon, nombre de segments
+const headTorchMaterial = new THREE.MeshStandardMaterial({
+  color: 0xff0000,
+  transparent: true,
+  roughness: 0.5, // brillance
+  metalness: 0.5, // métal
+  opacity: 0.5,
+  side: THREE.DoubleSide
+})
+const headTorch1 = new THREE.Mesh( headTorchGeometry, headTorchMaterial );
+headTorch1.position.x = 24.9
+headTorch1.position.y = 4
+headTorch1.position.z = 24.9
+torch.add( headTorch1 );
+
+const headTorch2 = new THREE.Mesh( headTorchGeometry, headTorchMaterial );
+headTorch2.position.x = -24.9
+headTorch2.position.y = 4
+headTorch2.position.z = 24.9
+torch.add( headTorch2 );
+
+const headTorch3 = new THREE.Mesh( headTorchGeometry, headTorchMaterial );
+headTorch3.position.x = 24.9
+headTorch3.position.y = 4
+headTorch3.position.z = -24.9
+torch.add( headTorch3 );
+
+const headTorch4 = new THREE.Mesh( headTorchGeometry, headTorchMaterial );
+headTorch4.position.x = -24.9
+headTorch4.position.y = 4
+headTorch4.position.z = -24.9
+torch.add( headTorch4 );
+
+// torch light
+const torchLight = new THREE.PointLight(0xffffff, 1, 15) // couleur, intensité, distance
+torchLight.position.set(24.9, 4, 24.9)
+scene.add(torchLight)
+
+// torch2 light
+const torch2Light = new THREE.PointLight(0xffffff, 1, 15)
+torch2Light.position.set(-24.9, 4, 24.9)
+scene.add(torch2Light)
+
+// torch3 light
+const torch3Light = new THREE.PointLight(0xffffff, 1, 15)
+torch3Light.position.set(24.9, 4, -24.9)
+scene.add(torch3Light)
+
+// torch4 light
+const torch4Light = new THREE.PointLight(0xffffff, 1, 15)
+torch4Light.position.set(-24.9, 4, -24.9)
+scene.add(torch4Light)
+
+// torch light top
+const torchLightTop = new THREE.PointLight(0xff0000, 1, 15)
+torchLightTop.position.set(24.9, 6, 24.9)
+scene.add(torchLightTop)
+
+// torch2 light top
+const torch2LightTop = new THREE.PointLight(0xff0000, 1, 15)
+torch2LightTop.position.set(-24.9, 6, 24.9)
+scene.add(torch2LightTop)
+
+// torch3 light top
+const torch3LightTop = new THREE.PointLight(0xff0000, 1, 15)
+torch3LightTop.position.set(24.9, 6, -24.9)
+scene.add(torch3LightTop)
+
+// torch4 light top
+const torch4LightTop = new THREE.PointLight(0xff0000, 1, 15)
+torch4LightTop.position.set(-24.9, 6, -24.9)
+scene.add(torch4LightTop)
+
+/**
  * Particles
  */
 // Geometry
@@ -385,7 +502,11 @@ particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
     size: 0.2,
-    sizeAttenuation: true
+    sizeAttenuation: true,
+    alphaMap: alphaMap,
+    transparent: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending
 })
 
 // Points
@@ -394,7 +515,7 @@ scene.add(bubbles)
 
 
 // Lights
-const ambientLight = new THREE.AmbientLight( 0xff9000, 0.3 ) // lumière ambiante, couleur, intensité de 0.5
+const ambientLight = new THREE.AmbientLight( 0xff9000, 0.5 ) // lumière ambiante, couleur, intensité de 0.5
 scene.add(ambientLight)
 
 const pointLight = new THREE.PointLight( 0xff9000, 1 )
@@ -405,6 +526,10 @@ pointLight.shadow.camera.far = 35 // distance maximale de la caméra
 pointLight.shadow.mapSize.width = 1024 // taille de la carte d'ombre
 pointLight.shadow.mapSize.height = 1024 // taille de la carte d'ombre
 scene.add(pointLight)
+
+const UpsidePointLight = new THREE.PointLight( 0xff9000, 1 )
+UpsidePointLight.position.set(0, -25, 0) // position de la lumière directionnelle sur l'axe x, y et z
+scene.add(UpsidePointLight)
 
 const pointLight2 = new THREE.PointLight( 0xff0000, 0.8 )
 pointLight2.position.set(0, 1, -20) // position de la lumière directionnelle sur l'axe x, y et z
@@ -449,13 +574,13 @@ scene.add(pointLight5)
 // const pointLightCameraHelper2 = new THREE.CameraHelper(pointLight2.shadow.camera)
 // scene.add(pointLightCameraHelper2)
 
-// Debug
-gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001).name('Ambient Light')
-gui.add(pointLight, 'intensity').min(0).max(1).step(0.001).name('Point Light')
-gui.add(pointLight2, 'intensity').min(0).max(1).step(0.001).name('Point Light 2')
-gui.add(pointLight3, 'intensity').min(0).max(1).step(0.001).name('Point Light 3')
-// gui.add(pointLight4, 'intensity').min(0).max(1).step(0.001).name('Point Light 4')
-gui.add(pointLight5, 'intensity').min(0).max(1).step(0.001).name('Point Light 5')
+// // Debug
+// gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001).name('Ambient Light')
+// gui.add(pointLight, 'intensity').min(0).max(1).step(0.001).name('Point Light')
+// gui.add(pointLight2, 'intensity').min(0).max(1).step(0.001).name('Point Light 2')
+// gui.add(pointLight3, 'intensity').min(0).max(1).step(0.001).name('Point Light 3')
+// // gui.add(pointLight4, 'intensity').min(0).max(1).step(0.001).name('Point Light 4')
+// gui.add(pointLight5, 'intensity').min(0).max(1).step(0.001).name('Point Light 5')
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
@@ -480,6 +605,28 @@ const tick = () =>
     const pointLight2Angle = elapsedTime * 0.5
     pointLight2.position.x = Math.cos(pointLight2Angle) * 10
     pointLight2.position.z = Math.sin(pointLight2Angle) * 10
+
+    // Torch
+    //faire clignoter torch1 et headTorch1
+    if (elapsedTime % 1 < 0.1) {
+        torchLightTop.visible = true
+        headTorch1.visible = true
+        torch2LightTop.visible = true
+        headTorch2.visible = true
+        torch3LightTop.visible = true
+        headTorch3.visible = true
+        torch4LightTop.visible = true
+        headTorch4.visible = true
+    } else {
+        torchLightTop.visible = false
+        headTorch1.visible = false
+        torch2LightTop.visible = false
+        headTorch2.visible = false
+        torch3LightTop.visible = false
+        headTorch3.visible = false
+        torch4LightTop.visible = false
+        headTorch4.visible = false
+    }
 
     // Update controls
     controls.update()
